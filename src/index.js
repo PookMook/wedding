e0dae56d7b6607e4d5aecf66a9962d03325ddafc
PookMook <arthur.juchereau@gmail.com>
2016-05-14 22:19:38 -0400

    Ca avance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Create de database
 var fs = require("fs");
 var file = "database.sqlite3";
 var exists = fs.existsSync(file);
@@ -14,27 +15,148 @@ db.serialize(function() {
 
   //First start of the app, create tables + infos
   if(!exists) {
-    db.run("CREATE TABLE invite (id INTEGER PRIMARY KEY, name TEXT, status INTEGER DEFAULT 0, code TEXT)");
-    var stmt = db.prepare("INSERT INTO invite (`name`, `code`) VALUES (?,?)");
 
-  //insert invite here
-      stmt.run("Jean Bérubé","joje872");
-      stmt.run("Josée Lauzon","joje872");
-      stmt.run("Sylvie Célérier","sylvie521");
-      stmt.run("Cyril Juchereau","Cyril92813");
-    stmt.finalize();
+    //Create invite table for your guests
+    db.run("CREATE TABLE invite (id INTEGER PRIMARY KEY, name TEXT, status INTEGER DEFAULT 0, code TEXT, admin INTEGER DEFAULT 0)");
+    var admin = db.prepare("INSERT INTO invite (`name`, `code`, `admin`) VALUES (?,?,?)");
+
+    //create gallery table for pictures of the wedding
+    db.run("CREATE TABLE coverPics (id INTEGER PRIMARY KEY, picture TEXT)");
+
+    //create gallery table for pictures of the wedding
+    db.run("CREATE TABLE gallery (id INTEGER PRIMARY KEY, picture TEXT, unpublish INTEGER DEFAULT 0, code TEXT, time INTEGER)");
+
+    //Create guestbook table
+    db.run("CREATE TABLE guestbook (id INTEGER PRIMARY KEY, text TEXT, unpublish INTEGER DEFAULT 0, code TEXT)");
+
+
+  //insert admin here
+      admin.run("Arthur","MyPassword",1);
+      admin.run("Catherine","MyPassword",1);
+    admin.finalize();
+
   }
 
+  //Prepared statement that checks userCode
 
   db.each("SELECT rowid AS id, name,code FROM invite", function(err, row) {
     console.log("Invites : " + row.id + ": " + row.name + ": " + row.code);
   });
 });
 
-db.close();
+//All prepared statement
+var checkCode = db.prepare("SELECT id,name FROM invite WHERE code = ?");
+var addPicture = db.prepare("INSERT INTO gallery (`picture`,`code`,`time`) VALUES (?,?,?)");
 
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+
+
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use('/side', express.static(__dirname + '/side'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/upload/cover', function(req, res) {
+  var cover = 'cover.jpg';
+  io.sockets.emit('newCover', {cover : cover});
+})
+app.post('/upload/pictures', function(req, res) {
+  res.send('hello world');
+  var picture = 'cover.jpg';
+  io.sockets.emit('newPicture', {picture : picture});
+})
+
+app.post('/upload/picture', function(req, res){
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+  var allowed = false;
+  var who = "";
+  form.on('field', function(name, value) {
+      if(name == "code"){
+        checkCode.each(value,function(err,row){
+          if(who == ""){
+            who = row.name;
+          }
+          else{
+            who += " & "+row.name;
+          }
+          allowed = true;
+        },function(){
+          form.on('file', function(field, file) {
+            if(allowed){
+              console.log(value + " has uploaded a picture");
+              fileName = makeid()+(Date.now()/1000)+file.name;
+              addPicture.run(fileName,value,Date.now());
+              fs.rename(file.path, path.join(form.uploadDir, fileName));
+              io.sockets.emit('newPicture', {picture : fileName, who : who, time : Date.now()});
+            }
+            else{
+              console.log(value + " tried to upload a picture but was bounced");
+              fs.unlink(file.path);
+            }
+          });
+        });
+      }
+    });
+  // every time a file has been uploaded successfully,
+  // rename it to a timestamp + random number + it's orignal name
+
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+});
+
+var clients = 0;
+io.on('connection', function(socket) {
+    clients++;
+    socket.emit('announcements', { message: 'A new user has joined!' });
+    io.sockets.emit('userCount', {clients : clients});
+    socket.on('event', function(data) {
+        console.log('A client sent us this dumb message:', data.message);
+    });
+    socket.on('disconnect', function(data) {
+        clients--;
+        io.sockets.emit('userCount', {clients : clients});
+        console.log('Client quit :'+clients);
+    });
+    console.log('New client'+clients);
+});
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 10; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+
+
+server.listen(8080);
--- src/side/client.js
+++ src/side/client.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+
+
+  //recover code from URL
+  var code;
+  if(document.location.toString().indexOf('?') !== -1) {
+    var query = document.location.toString().replace(/^.*?\?/, '').replace(/#.*$/, '').split('&');
+    for(var i=0, l=query.length; i<l; i++) {
+
+       aux = decodeURIComponent(query[i]).split('=');
+             if(aux[0] == "code"){
+               code = aux[1];
+             }
+      }
+      delete aux;
+}
+
+
   $("#cover").on("click",function(){
      if($(".slideshow.active").next(".slideshow").is("div")){
           $(".slideshow.active").removeClass("active").next(".slideshow").addClass("active");
@@ -10,7 +27,9 @@ $(document).ready(function(){
   });
 
   setInterval(function(){$("#cover").trigger("click");},5000);
-  $("#cover").css("height",$("#cover").innerHeight());
+  $("#cover").css("height",window.innerHeight+"px");
+
+
   $(window).on("scroll",function(){
     if ($(window).scrollTop()+1 > $("#cover").innerHeight())
     {$("body > nav").addClass("page");}
@@ -18,9 +37,61 @@ $(document).ready(function(){
       $("body > nav").removeClass("page");
     }
   });
+
+
   $("nav > a").on("click",function(e){
     e.preventDefault();
     $('html,body').animate({
  scrollTop : $("#"+$(this).data("href")).offset().top},'slow');
   });
+
+
+  //Socket management
+  var socket = io.connect('/');
+    socket.emit('event', { message: 'Hey, I have an important message!' });
+    socket.on('announcements', function(data) {
+        console.log('Got announcement:', data.message);
+    });
+    socket.on('userCount', function(data) {
+        console.log('Update number of user:', data.clients);
+    });
+    socket.on('newCover', function(data) {
+        console.log('Update cover pic:', data.cover);
+    });
+    socket.on('newPicture', function(data) {
+        console.log('Add pic:', data.picture);
+        $item = $('<figure class="grid-item"><img src="/uploads/'+data.picture+'" data-who="'+data.who+'" data-time="'+data.time+'"></figure>');
+      $(".grid").prepend( $item );
+
+    });
+
+
+    $('#uploadPicture').on('change', function(){
+
+      var files = $(this).get(0).files;
+
+      if (files.length > 0){
+        // One or more files selected, process the file upload
+          var formData = new FormData();
+          // loop through all the selected files
+          for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            // add the files to formData object for the data payload
+            formData.append('uploads[]', file, file.name);
+          }
+          formData.append('code',code);
+          $.ajax({
+          url: '/upload/picture',
+          type: 'POST',
+          data: formData,
+          processData: false,
+          contentType: false,
+          success: function(data){
+              console.log('upload successful!');
+          }
+        });
+      }
+
+    });
+
 });
